7348e24f861dba1ae086f314708d38a54dbbd486
Sylvia Chebet <sylviachebet03@gmail.com>
2024-10-14 23:25:12 +0300

    Populate the database with the hospitals data exported from QGIS in geoJSON format.

--- backend-biashara-first/populate_hospitals.py
+++ backend-biashara-first/populate_hospitals.py
@@ -2,7 +2,6 @@ import json
 from models import db, Hospital
 from app import app
 
-# Path to your geoJSON file
 geojson_file ='./Hospital_locations.geojson'
 
 # Load the geoJSON data
@@ -14,8 +13,8 @@ with app.app_context():
     # Iterate over the features in the geoJSON file
     for feature in geojson_data['features']:
         hospital_name = feature['properties'].get('name')
-        hospital_address = feature['properties'].get('address', 'Unknown')  # Default to 'Unknown' if no address
-        latitude = feature['geometry']['coordinates'][1]  # GeoJSON stores longitude first, latitude second
+        hospital_address = feature['properties'].get('address', 'Unknown')  
+        latitude = feature['geometry']['coordinates'][1]
         longitude = feature['geometry']['coordinates'][0]
         services = feature['properties'].get('services', 'No services listed')
         contact_info = feature['properties'].get('contact_info', 'No contact info')
@@ -30,10 +29,10 @@ with app.app_context():
             contact_info=contact_info
         )
 
-        # Add the hospital to the session
+       
         db.session.add(new_hospital)
 
-    # Commit the session to the database
+    
     db.session.commit()
 
 print("Hospitals have been successfully added to the database!")
